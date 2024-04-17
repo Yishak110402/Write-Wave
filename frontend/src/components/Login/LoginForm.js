3974a663e4748e29a7bc208acb20ea574793ac80
yishak <yishak.110402@gmail.com>
2024-04-17 16:22:45 +0300

    first commit

--- frontend/src/components/Login/LoginForm.js
+++ frontend/src/components/Login/LoginForm.js
@@ -3,44 +3,55 @@ import blogImage from "./../../assets/undraw_Blog_post_re_fy5x.png";
 import "./LoginForm.css";
 import { Link, useNavigate } from "react-router-dom";
 
-export default function LoginForm({activeUser, setActiveUser}) {
+export default function LoginForm({ activeUser, setActiveUser }) {
   const [formData, setFormData] = useState({});
   const [showErrorMsg, setShowErrorMsg] = useState(false);
-  const [err, setErr] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const navigate = useNavigate()
+  const [err, setErr] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
- useEffect(function(){
-  if(activeUser){
-    navigate('/feed')
-  }
- },[activeUser])
+  useEffect(
+    function () {
+      if (activeUser) {
+        navigate("/feed");
+      }
+    },
+    [activeUser]
+  );
 
   async function handleFormSubmit(e) {
     e.preventDefault();
-    setLoading(true)
-    setShowErrorMsg(false)
-    setErr(null)
+    setLoading(true);
+    setShowErrorMsg(false);
+    setErr(null);
 
-    const res = await fetch("https://writewave-backend-api.onrender.com/users/login", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
+    const res = await fetch(
+      "https://writewave-backend-api.onrender.com/users/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
+    if (!res) {
+      setErr("Failed fetching data");
+      setShowErrorMsg(true);
+      setLoading(false);
+      return;
+    }
     const data = await res.json();
     console.log(data);
-    if(data.status === "fail"){
-      setErr(data.message)
-      setLoading(false)
-      setShowErrorMsg(true)
-      return
+    if (data.status === "fail") {
+      setErr(data.message);
+      setLoading(false);
+      setShowErrorMsg(true);
+      return;
     }
-    setLoading(false)
-    setActiveUser(data.data.user)
-    navigate('/feed')
-
+    setLoading(false);
+    setActiveUser(data.data.user);
+    navigate("/feed");
   }
   return (
     <div className="login">
@@ -48,7 +59,9 @@ export default function LoginForm({activeUser, setActiveUser}) {
       <form onSubmit={(e) => handleFormSubmit(e)}>
         <h1>Write Wave</h1>
         {showErrorMsg && (
-          <div className="error-msg">{err ? err : "Error while signing up"}</div>
+          <div className="error-msg">
+            {err ? err : "Error while signing up"}
+          </div>
         )}
         <input
           onChange={(e) => {
@@ -66,8 +79,12 @@ export default function LoginForm({activeUser, setActiveUser}) {
           required
           placeholder="Password"
         />
-        <button disabled = {loading} type="submit">{!loading?"Log in": "Loading"}</button>
-        <p>Don't have an account <Link to='/signup'>sign up</Link> </p>
+        <button disabled={loading} type="submit">
+          {!loading ? "Log in" : "Loading"}
+        </button>
+        <p>
+          Don't have an account <Link to="/signup">sign up</Link>{" "}
+        </p>
       </form>
     </div>
   );
